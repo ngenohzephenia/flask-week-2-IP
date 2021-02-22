6d72db5cc464c98254a856f16400eeb73779da4a
ngenohzephenia <ngenohzef@gmail.com>
2021-02-22 12:34:26 +0300

    create first call API

--- app/request.py
+++ app/request.py
@@ -1,4 +1,9 @@
 from app import app
+from  urllib.request,jason
+from models import news
+
+News =news.News
 
 # Getting api key
 api_key =app.config['461ad7724e7445dcb49c72ba517bfdbe']
+base_url = app.config["News_API_BASE_URL"]
