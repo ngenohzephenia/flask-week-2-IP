42c51401eafa157d970ed9687796d19811c21b01
ngenohzephenia <ngenohzef@gmail.com>
2021-02-22 09:59:17 +0300

    create a request.py and import app instance

--- app/config.py
+++ app/config.py
@@ -2,7 +2,7 @@ class Config:
     '''
     General configuration parent class
     '''
-    pass
+    NEWS_API_BASE_URL ='https://newsapi.org/register/success/{}?api_key={}'
 
 
 
---
+++ app/request.py
@@ -0,0 +1,4 @@
+from app import app
+
+# Getting api key
+api_key =app.config['461ad7724e7445dcb49c72ba517bfdbe']
