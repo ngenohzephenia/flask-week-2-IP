bf0277caae1477274d23092435dfda49e353da4f
ngenohzephenia <ngenohzef@gmail.com>
2021-02-22 12:50:24 +0300

    create processing function

--- app/request.py
+++ app/request.py
@@ -25,4 +25,30 @@ def get_news(category):
             news_results = process_results(news_results_list)
 
 
-    return movie_results
+
+
+def process_results(news_list):
+    '''
+    Function  that processes the news result and transform them to a list of Objects
+
+    Args:
+        news_list: A list of dictionaries that contain news details
+
+    Returns :
+        news_results: A list of news objects
+    '''
+    news_results = []
+    for news_item in news_list:
+        id = news_item.get('id')
+        title = news_item.get('original_title')
+        overview = news_item.get('overview')
+        poster = news_item.get('poster_path')
+        
+
+        if poster:
+            news_object = News(id,title,overview,poster)
+            news_results.append(news_object)
+
+    
+
+    return news_results
